662756590cd128c81021cca6abc42a4015bd38e3
Carlos Barahona <carlos_nscl@hotmail.com>
2025-11-19 01:10:16 -0300

    remove duplicate condition

--- src/memory.rs
+++ src/memory.rs
@@ -520,7 +520,6 @@ fn get_boss_state(game: &Process, addresses: &Memory, level_id: &GameStage) -> u
         || *level_id == GameStage::Stage4_4Past
         || *level_id == GameStage::Stage5_4Past
         || *level_id == GameStage::Stage6_4Past
-        || *level_id == GameStage::Stage6_5
         || *level_id == GameStage::StageSonic3
     {
         boss_state = addresses
